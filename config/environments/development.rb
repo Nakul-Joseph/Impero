d5bf79027588900677283fb2061869322fa58a29
nakul <nakuljsph@gmail.com>
2021-03-08 18:20:53 +0000

    Add hosts and configs for aws cloud9

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "138a2a9acdd34974827b27aeed7bad35.vfs.cloud9.eu-west-1.amazonaws.com"
 end
